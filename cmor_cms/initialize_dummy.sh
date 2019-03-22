5f41df8feb00f75cc1e2a9e5fe953fbefe184e26
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-03-22 10:10:00 +0100

    Adding spec prepare scripts for all subgems.

---
+++ cmor_cms/initialize_dummy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+GEM_NAME=${PWD##*/}
+INSTALL_NAME=${GEM_NAME//cmor_/cmor\:}
+
+# Delete old dummy app
+rm -rf spec/dummy
+
+# Generate new dummy app
+DISABLE_MIGRATE=true rake dummy:app
+rm spec/dummy/.ruby-version
+
+# Satisfy prerequisites
+cd spec/dummy
+rails g model User email
+rails active_storage:install
+touch config/initializers/i18n.rb
+echo "Rails.application.config.i18n.available_locales = [:en, :de]" >> config/initializers/i18n.rb
+echo "Rails.application.config.i18n.default_locale    = :de" >> config/initializers/i18n.rb
+
+# Install
+rails generate $INSTALL_NAME:install
+rails $GEM_NAME:install:migrations db:migrate db:test:prepare
