7f7036a6f937624d9d797d9987a15c1a9a773e5f
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-03-28 15:20:11 +0100

    Cmor: Set correct gems in gem publish script.

--- publish_all.sh
+++ publish_all.sh
@@ -3,14 +3,16 @@ shopt -s expand_aliases
 source ~/.bash_aliases
 
 echo $PWD
-for i in blog cms2 contact2 core files galleries links2 rbac sliders2 tags user_area2; do
+publish_gem
+
+for i in blog carousels cms contact core files galleries links rbac suite tags user_area; do
   cd "cmor_${i}"
   echo $PWD
   publish_gem
   cd ..
 done
 
-for i in blog cms2 contact2 core files galleries links2 rbac sliders2 suite tags user_area2; do
+for i in blog carousels cms contact core files galleries links rbac tags user_area; do
   cd "cmor_${i}_backend"
   echo $PWD
   publish_gem
