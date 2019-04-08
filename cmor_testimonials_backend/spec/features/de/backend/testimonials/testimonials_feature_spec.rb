74f5550d76521051144bbcc48a93b073ca990de6
Eugen Mueller <eugen.mueller@enermarket.de>
2019-04-08 14:25:09 +0200

    Fix image update.

--- cmor_testimonials_backend/spec/features/de/backend/testimonials/testimonials_feature_spec.rb
+++ cmor_testimonials_backend/spec/features/de/backend/testimonials/testimonials_feature_spec.rb
@@ -42,18 +42,22 @@ RSpec.describe '/de/backend/testimonials/testimonials', type: :feature do
     }
   end
 
-  describe 'replacing image' do
+  describe 'replacing image on update' do
     let(:resource) { create(:cmor_testimonials_testimonial)}
     let(:base_path) { '/de/backend/testimonials/testimonials' }
     let(:edit_path) { "#{base_path}/#{resource.to_param}/edit" }
+    let(:filename) { "enerbunny.png" }
 
     let(:submit_button) { within('form.edit_testimonial') { first('input[type="submit"]') } }
     
     before(:each) do
       visit(edit_path)
-      attach_file 'testimonial[overwrite_image][]', Cmor::Testimonials::Engine.root.join(*%w( spec files cmor testimonials testimonials enerbunny.png))
+      attach_file 'testimonial[image]', Cmor::Testimonials::Engine.root.join(*%W( spec files cmor testimonials testimonials #{filename}))
+      submit_button.click
+      resource.reload;
     end
-    it { expect{ submit_button.click  }.to change{ resource.image } }
+    
+    it { expect(resource.image.filename.to_s).to eq(filename) }
   end
 
 end
