5d5dad9373b881fa3e7576deb714d81c11ea3c4a
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-04-15 22:54:28 +0200

    Cmor::Core: Rename MarkupViewHelper #render to #to_html. Cmor::Carousels: Use #to_html on MarkupViewHelper instead of #render.

--- cmor_core/app/view_helpers/cmor/core/markup_view_helper.rb
+++ cmor_core/app/view_helpers/cmor/core/markup_view_helper.rb
@@ -1,7 +1,16 @@
 module Cmor
   module Core
+    # Example:
+    #
+    #     # app/controllers/application_controller.rb
+    #     class ApplicationController < ActionController::Base
+    #       view_helper Cmor::Core::MarkupViewHelper, as: :markup_helper
+    #     end
     class MarkupViewHelper < Rao::ViewHelper::Base
-      def render(object, method)
+      # Example:
+      #
+      #     = markup_helper(self).to_html(post, :body)
+      def to_html(object, method)
         handler = object.send(method)
         return if handler.nil?
         erb_string = ::ERB.new(object[method.to_sym].to_s, 0).result(binding)
