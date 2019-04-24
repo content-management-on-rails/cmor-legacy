cc698d1f212a7f03b5235c822cfcd316910dc780
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-04-24 23:26:59 +0200

    Cmor::Links: Fix specs.

--- cmor_links/spec/view_helpers/cmor/links/application_view_helper_spec.rb
+++ cmor_links/spec/view_helpers/cmor/links/application_view_helper_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe Cmor::Links::ApplicationViewHelper, type: :view_helper do
   include ActionView::RecordIdentifier
 
   describe "render_footer" do
-    it { expect(rendered).to have_css(".link-footer") }
+    it { expect(html).to have_css(".link-footer") }
   end
 
   describe "render_category" do
     let(:category) { create(:cmor_links_category) }
     let(:options) { category }
 
-    it { expect(rendered).to have_content(category.name) }
-    it { expect(rendered).to have_css("##{dom_id(category)}") }
+    it { expect(html).to have_content(category.name) }
+    it { expect(html).to have_css("##{dom_id(category)}") }
   end
 
   describe "render_link" do
     let(:link) { create(:cmor_links_link) }
     let(:options) { link }
 
-    it { expect(rendered).to have_link(link.name, href: link.url) }
+    it { expect(html).to have_link(link.name, href: link.url) }
   end
 end
