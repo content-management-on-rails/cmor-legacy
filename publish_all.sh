b0c6b1a4f47c527f828f404c091f7fd98c5b7a83
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-04-25 15:23:03 +0200

    Add cmor_core_frontend, cmor_testimonials and cmor_testimonials_backend to gem publishing script.

--- publish_all.sh
+++ publish_all.sh
@@ -5,14 +5,14 @@ source ~/.bash_aliases
 echo $PWD
 publish_gem
 
-for i in blog carousels cms contact core files galleries links rbac suite tags user_area; do
+for i in blog carousels cms contact core core_frontend files galleries links rbac suite testimonials tags user_area; do
   cd "cmor_${i}"
   echo $PWD
   publish_gem
   cd ..
 done
 
-for i in blog carousels cms contact core files galleries links rbac system tags user_area; do
+for i in blog carousels cms contact core files galleries links rbac system testimonials tags user_area; do
   cd "cmor_${i}_backend"
   echo $PWD
   publish_gem
