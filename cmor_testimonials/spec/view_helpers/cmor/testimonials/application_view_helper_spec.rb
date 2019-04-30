87792494675519d04f84c2a51fc9d765364e4269
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-04-30 17:33:34 +0200

    Cmor::Testimonials: Let ApplicationViewHelper#render_category accept a category as first argument. Closes #19 Cmor::Testimonials: Fix double arrows in ApplicationViewHelper#render_category. Cmor::Testimonials: Remove obsolete code. Cmor::Testimonials: Fix missing translations.

--- cmor_testimonials/spec/view_helpers/cmor/testimonials/application_view_helper_spec.rb
+++ cmor_testimonials/spec/view_helpers/cmor/testimonials/application_view_helper_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Cmor::Testimonials::ApplicationViewHelper, type: :view_helper do
-  let(:category) { create(:cmor_testimonials_category, testimonials: testimonials) }
-  let(:args) { [category.identifier, {}] }
-
   before(:each) do
     register_view_helper Markup::Rails::ApplicationViewHelper, as: :markup_helper
   end
   
   context 'testimonials present' do
     let(:testimonials) { create_list(:cmor_testimonials_testimonial, 3) }
+    let(:category) { create(:cmor_testimonials_category, testimonials: testimonials) }
+    let(:args) { [category.identifier, {}] }
 
     describe 'render_category' do
       it { expect(html).to have_css('div.cmor-testimonials-category') }
@@ -18,9 +17,21 @@ RSpec.describe Cmor::Testimonials::ApplicationViewHelper, type: :view_helper do
 
   context 'testimonials absent' do
     let(:testimonials) {[]}
+    let(:category) { create(:cmor_testimonials_category, testimonials: testimonials) }
+    let(:args) { [category.identifier, {}] }
 
     describe 'render_category' do
       it { expect(rendered).to eq(nil) }
+    end
+  end
+
+  context 'when using a category as first argument' do
+    let(:testimonials) { create_list(:cmor_testimonials_testimonial, 3) }
+    let(:category) { create(:cmor_testimonials_category, testimonials: testimonials) }
+    let(:args) { [category, {}] }
+
+    describe 'render_category' do
+      it { expect(html).to have_css('div.cmor-testimonials-category') }
     end
   end
 end
