fb77149805859afcd74ef623f82e04c09dcbef06
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-06-12 14:09:23 +0200

    Use bundle exec when running specs.

--- rspec-all.sh
+++ rspec-all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -l
-for d in ./cmor_*/ ; do (cd "$d" && ./initialize_dummy.sh && rspec -f d); done
+for d in ./cmor_*/ ; do (cd "$d" && ./initialize_dummy.sh && bundle exec rspec -f d); done
