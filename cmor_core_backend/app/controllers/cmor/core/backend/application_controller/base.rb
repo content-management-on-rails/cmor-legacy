340f813aaafddc2fa2d5eeb134911451596d4955
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-06-19 20:40:02 +0200

    Cmor::Core::Backend: Add base application controller (Cmor::Core::Backend::ApplicationController::Base) to be used as base controller by backend modules for arbitrary use (for controllers that are neither resources nor service controllers).

---
+++ cmor_core_backend/app/controllers/cmor/core/backend/application_controller/base.rb
@@ -0,0 +1,10 @@
+module Cmor
+  module Core
+    module Backend
+      module ApplicationController
+        class Base < Administrador::ApplicationController
+        end
+      end
+    end
+  end
+end
