2589d9c741af400cfe9f475cc15c165227573567
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-06-28 10:39:39 +0200

    Bumped version to 0.0.15.pre

--- publish-all.sh
+++ publish-all.sh
@@ -5,7 +5,7 @@ source ~/.bash_aliases
 echo $PWD
 publish_gem
 
-for i in blog carousels cms contact core core_frontend files galleries links rbac suite testimonials tags user_area; do
+for i in blog carousels cms contact core core_frontend files galleries links partners rbac suite testimonials tags user_area; do
   cd "cmor_${i}"
   echo $PWD
   publish_gem
@@ -14,6 +14,13 @@ done
 
 for i in blog carousels cms contact core files galleries links rbac system testimonials tags user_area; do
   cd "cmor_${i}_backend"
+  echo $PWD
+  publish_gem
+  cd ..
+done
+
+for i in partners; do
+  cd "cmor_${i}_frontend"
   echo $PWD
   publish_gem
   cd ..
