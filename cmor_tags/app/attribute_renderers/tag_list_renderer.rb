21e08f3a6e36ac149b385765b6229089af3bede8
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-07-21 15:50:49 +0200

    Cmor::Tags: Cleanup.

--- cmor_tags/app/attribute_renderers/tag_list_renderer.rb
+++
@@ -1,8 +0,0 @@
-class TagListRenderer < ResourceRenderer::AttributeRenderer::Base
-  def display(attribute_name, _label, _options = {}, &_block)
-    tag_list = value_for_attribute(attribute_name)
-    output = tag_list.collect do |tag|
-      h.content_tag(:span, tag, class: 'label label-default')
-    end.join('&nbsp;').html_safe
-  end
-end
--- cmor_tags/lib/cmor_tags.rb
+++ cmor_tags/lib/cmor_tags.rb
@@ -4,17 +4,3 @@ require 'rao-service'
 require 'rao-service_controller'
 
 require 'cmor/tags'
-
-# require 'acts_as_taggable_on/tag'
-# module ActsAsTaggableOn
-#   class Tag
-#     self.table_name = 'cmor_tags_tags'
-#   end
-# end
-
-# require 'acts_as_taggable_on/tagging'
-# module ActsAsTaggableOn
-#   class Tagging
-#     self.table_name = 'cmor_tags_taggings'
-#   end
-# end
