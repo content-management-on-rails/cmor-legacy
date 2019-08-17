8238d0e49319b7c9a6bd7faa7e8906a546e0ff5f
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-08-17 12:38:26 +0200

    Cmor::Carousels: Add video_html_options to ApplicationViewHelper#render_carousel.

--- cmor_carousels/app/view_helpers/cmor/carousels/application_view_helper.rb
+++ cmor_carousels/app/view_helpers/cmor/carousels/application_view_helper.rb
@@ -14,8 +14,9 @@ module Cmor
       #     = carousels_helper(self).render_carousel(:main, autostart: true, interval: 5.0, variant_options: { resize: "1920x1080" })
       #
       def render_carousel(identifier, options = {})
-        options.reverse_merge!(autostart: true, interval: 5.0, keyboard: true, pause: :hover, ride: false, wrap: true, controls: true, indicators: true)
+        options.reverse_merge!(autostart: true, interval: 5.0, keyboard: true, pause: :hover, ride: false, wrap: true, controls: true, indicators: true, video_html_options: {})
         carousel_options = options.slice(:autostart, :interval, :pause, :ride, :wrap)
+        video_html_options = options.delete(:video_html_options).reverse_merge!(autoplay: true, muted: true)
         data_attributes = carousel_options_to_data_attributes(carousel_options)
 
         carousel = Cmor::Carousels::Carousel.where(identifier: identifier.to_s).for_locale(I18n.locale).first
@@ -24,7 +25,7 @@ module Cmor
           return I18n.t('cmor.carousels.carousel.warnings.not_found', identifier: identifier)
         else
           variant_options = options.delete(:variant_options) || carousel.variant_options
-          render carousel: carousel, options: options, data_attributes: data_attributes, variant_options: variant_options
+          render carousel: carousel, options: options, data_attributes: data_attributes, variant_options: variant_options, video_html_options: video_html_options
         end
       end
 
