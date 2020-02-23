9715c0bde37020fa0202e6d5ffb61b99b045a5d5
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-02-23 17:00:12 +0100

    Cmor::Galleries: Add missing require.

--- cmor_galleries/lib/cmor_galleries.rb
+++ cmor_galleries/lib/cmor_galleries.rb
@@ -6,5 +6,6 @@ require 'rao-resources_controller'
 require 'rao-view_helper'
 require 'haml-rails'
 
+require 'cmor/core/model/collection_concern'
 require 'cmor/core/model/localization_concern'
 require 'cmor/galleries'
