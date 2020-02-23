8a9f55b5af623cbfac418baf1f09e5bb1a299982
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-02-23 15:18:10 +0100

    Cmor::Galleries::Backend: Add locale to picture galleries. Cmor::Galleries::Backend: Make picture galleries positionable. Cmor::Galleries::Backend: Make picture galleries sluggable. Cmor::Galleries::Backend: Make picture galleries publishable.

--- cmor_galleries_backend/app/controllers/cmor/galleries/backend/picture_galleries_controller.rb
+++ cmor_galleries_backend/app/controllers/cmor/galleries/backend/picture_galleries_controller.rb
@@ -2,7 +2,9 @@ module Cmor
   module Galleries
     module Backend
       class PictureGalleriesController < Cmor::Core::Backend::ResourcesController::Base
+        include Rao::ResourcesController::FriendlyIdConcern
         include Rao::ResourcesController::ActsAsPublishedConcern
+        include Rao::ResourcesController::ActsAsListConcern
 
         helper Twitter::Bootstrap::Components::Rails::V4::ComponentsHelper
 
@@ -13,7 +15,7 @@ module Cmor
         private
 
         def permitted_params
-          params.require(:picture_gallery).permit(:name, :description, :published, { assets: [], overwrite_assets: [], append_assets: [] })
+          params.require(:picture_gallery).permit(:locale, :name, :description, :published, { assets: [], overwrite_assets: [], append_assets: [] })
         end
       end
     end
--- cmor_galleries_backend/config/routes.rb
+++ cmor_galleries_backend/config/routes.rb
@@ -1,12 +1,13 @@
 Cmor::Galleries::Backend::Engine.routes.draw do
   resources :picture_galleries do
-    post :toggle_published, on: :member
+    acts_as_list
+    acts_as_published
   end
 
   resources :picture_details, except: [:new] do
     post :destroy_many, on: :collection
-    post :reposition, on: :member
-    post :toggle_published, on: :member
+    acts_as_list
+    acts_as_published
   end
 
   root to: 'home#index'
