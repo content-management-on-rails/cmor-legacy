d73ef11c80d1f8c4899af0196cb6b7345e2255ee
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-03-23 12:52:58 +0100

    Cmor::Legal::Frontend: Improvements to the cookie banner UI. Cmor::Legal::Frontend: Add cookies_consent_banner_modal_class configuration option to set the css class of the modal. Cmor::Legal::Frontend: Add cookies_consent_banner_modal_dialog_class configuration option to set the css class of the modal dialog.

--- cmor_legal_frontend/lib/cmor/legal/frontend/configuration.rb
+++ cmor_legal_frontend/lib/cmor/legal/frontend/configuration.rb
@@ -9,6 +9,9 @@ module Cmor
         end
 
         mattr_accessor(:base_controller) { "::FrontendController" }
+
+        mattr_accessor(:cookies_consent_banner_modal_class) { 'modal fade cookies-eu js-cookies-eu' }
+        mattr_accessor(:cookies_consent_banner_modal_dialog_class) { 'modal-lg modal-dialog' }
       end
     end
   end
--- cmor_legal_frontend/lib/generators/cmor/legal/frontend/install/templates/initializer.rb
+++ cmor_legal_frontend/lib/generators/cmor/legal/frontend/install/templates/initializer.rb
@@ -4,4 +4,16 @@ Cmor::Legal::Frontend.configure do |config|
   # Default: config.base_controller = '<%= base_controller_class_name %>'
   #
   config.base_controller = '<%= base_controller_class_name %>'
+
+  # Set the css class for the cookie consent banner modal.
+  #
+  # Default: config.cookies_consent_banner_modal_class = 'modal fade cookies-eu js-cookies-eu'
+  #
+  config.cookies_consent_banner_modal_class = 'modal fade cookies-eu js-cookies-eu'
+
+  # Set the css class for the cookie consent banner modal dialog.
+  #
+  # Default: config.cookies_consent_banner_modal_dialog_class = 'modal-lg modal-dialog'
+  #
+  config.cookies_consent_banner_modal_dialog_class = 'modal-lg modal-dialog'
 end
