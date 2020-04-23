2e3d52d9381fd680ae24166c701f3fb226941461
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-04-23 12:11:15 +0200

    Cmor::MultiTenancy: Remove automatic has_many association on client.

--- cmor_multi_tenancy/app/concerns/cmor/multi_tenancy/model/client_concern.rb
+++ cmor_multi_tenancy/app/concerns/cmor/multi_tenancy/model/client_concern.rb
@@ -6,7 +6,6 @@ module Cmor
 
         included do
           belongs_to :client
-          Client.has_many self.name.demodulize.underscore.pluralize.to_sym, class_name: self.name
         end
       end
     end
