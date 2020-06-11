84821759fedcb019603905f46d00fe0fd48f238a
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-06-11 14:47:05 +0200

    Cmor::Files: Add ApplicationViewHelper#render_folder.

--- cmor_files/app/view_helpers/cmor/files/application_view_helper.rb
+++ cmor_files/app/view_helpers/cmor/files/application_view_helper.rb
@@ -11,7 +11,7 @@ module Cmor
       # Example:
       #
       #     # app/views/layouts/application.html.haml
-      #     = files_helper(self).render_download(identifier: 'Quick reference manual')
+      #     = files_helper(self).render_file(identifier: 'Quick reference manual')
       #
       def render_file(identifier:)
         resource = Cmor::Files::FileDetail.published.where(identifier: identifier).first
@@ -20,6 +20,24 @@ module Cmor
         else
           render resource: resource
         end
+      end
+
+      # Example:
+      #
+      #     # app/views/layouts/application.html.haml
+      #     = files_helper(self).render_folder(identifier: 'Manuals')
+      #
+      def render_folder(identifier:)
+        resource = Cmor::Files::Folder.published.where(identifier: identifier).first
+        if resource.nil?
+          "Could not find published folder with identifier: '#{identifier}'."
+        else
+          render resource: resource
+        end
+      end
+
+      def asset_download_button_label(asset)
+        "#{c.t(".download")} (#{asset.blob.content_type.split('/').last}, #{c.number_to_human_size(asset.blob.byte_size)})"
       end
     end
   end
