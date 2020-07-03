6965f2bcdae88cd44f36d43aee1d3a2888ba53c0
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-03 15:37:22 +0200

    Cmor::Cms: Fix glob routing being too greedy and trying to resolve active storage routes.

--- cmor_cms/config/routes.rb
+++ cmor_cms/config/routes.rb
@@ -1,6 +1,8 @@
 Cmor::Cms::Engine.routes.draw do
   localized do
-    get '/*page', to: 'page#respond', as: :page
+    get '/*page', to: 'page#respond', as: :page, constraints: lambda { |req|
+      req.path.exclude? 'rails/active_storage'
+    }
     get '/',      to: 'page#respond', page: 'home'
   end
 end
