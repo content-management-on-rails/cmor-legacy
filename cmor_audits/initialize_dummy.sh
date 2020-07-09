2c53e7eec70b1828a4bff6e767723fa872f6b694
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-09 12:43:28 +0200

    Cmor::Audits: Explicity require factory bot in the dummy app.

--- cmor_audits/initialize_dummy.sh
+++ cmor_audits/initialize_dummy.sh
@@ -37,7 +37,7 @@ echo "end" >> config/initializers/route_translator.rb
 
 # Add turbolinks
 sed -i "15irequire 'turbolinks'" config/application.rb
-# sed -i "16irequire 'factory_bot_rails'" config/application.rb
+sed -i "16irequire 'factory_bot_rails'" config/application.rb
 
 # Install administrador
 rails generate administrador:install
