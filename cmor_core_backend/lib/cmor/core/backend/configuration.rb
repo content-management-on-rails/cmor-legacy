4ff24d9935dffd4977bcf595267b530026991d61
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-12 01:59:58 +0200

    Cmor::Core::Backend: Add plugin system for resources routing via Cmor::Core::Backend::Configuration#add_resources_routes.

--- cmor_core_backend/lib/cmor/core/backend/configuration.rb
+++ cmor_core_backend/lib/cmor/core/backend/configuration.rb
@@ -16,8 +16,39 @@ module Cmor
           }
         }
 
+
         def self.image_variant_options_for(identifier)
           @@image_variant_options[identifier]
+        end
+
+        # You can add routes to resources based an any condition that can based
+        # on the controller or resource class that a route is used for.
+        #
+        # This is useful when writing modules that conditionally add actions to
+        # resources based on the capabilities of the controller or resource class.
+        #
+        # Example:
+        #
+        #     config.add_resources_routes(
+        #       :cmor_audits,
+        #       condition: ->{ resource_class.ancestors.map(&:to_s).include?("PaperTrail::Model::InstanceMethods") },
+        #       routes:    ->{ paper_trail_resources }
+        #     )
+        #
+        # Example:
+        #
+        #     config.add_resources_routes(
+        #       :acts_as_list,
+        #       condition: ->{ resource_class.respond_to?(:position) },
+        #       routes:    ->{ get :toggle_position, on: :member }
+        #     )
+        #
+        def add_resources_routes(name, options)
+          (@@resources_routes ||= {})[name] = options
+        end
+
+        def self.resources_routes
+          @@resources_routes || {}
         end
       end
     end
