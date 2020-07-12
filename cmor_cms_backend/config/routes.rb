378d1c2699ec782e18f12a32ffc169d3e4ac90c8
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-12 02:09:19 +0200

    Cmor::Cms::Backend: Add Cmor::Core::Backend resources routing plugin hooks.

--- cmor_cms_backend/config/routes.rb
+++ cmor_cms_backend/config/routes.rb
@@ -2,28 +2,38 @@ Cmor::Cms::Backend::Engine.routes.draw do
   resources :add_homepages_service, only: [:create, :new]
   resources :import_partials_service, only: [:create, :new]
 
-  resources :content_boxes
-  resources :navigations
+  resources :content_boxes do
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
+  end
+
+  resources :navigations do
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
+  end
   
   resources :navigation_items do
     post :toggle_published, on: :member
     post :reposition, on: :member
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
   end
   
   resources :layouts do
     post :toggle_published, on: :member
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
   end
 
   resources :pages do
     post :toggle_published, on: :member
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
   end
   
   resources :partials do
     post :toggle_published, on: :member
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
   end
   
   resources :templates do
     post :toggle_published, on: :member
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
   end
 
   root to: 'home#index'
