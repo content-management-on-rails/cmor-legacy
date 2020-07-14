974e08424cc4e2f7bcd8f06fc457ca0d2455c91d
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-14 21:05:48 +0200

    Cmor::Blog::Backend: Add Cmor::Audits integration.

--- cmor_blog_backend/config/routes.rb
+++ cmor_blog_backend/config/routes.rb
@@ -1,9 +1,11 @@
 Cmor::Blog::Backend::Engine.routes.draw do
   resources :posts do
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
     post :toggle_published, on: :member
     post :reposition, on: :member
   end
   resources :asset_details do
+    Cmor::Core::Backend::Routing::ResourcesRoutes.inject_routes(self)
     post :reposition, on: :member
   end
 
