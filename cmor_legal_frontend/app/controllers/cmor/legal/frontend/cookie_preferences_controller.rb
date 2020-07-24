be2924f8c9cfae3c083d334e22c4a51e9092e009
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-24 16:10:10 +0200

    Cmor::Legal::Frontend: Use last location as target url after updating cookie preferences.

--- cmor_legal_frontend/app/controllers/cmor/legal/frontend/cookie_preferences_controller.rb
+++ cmor_legal_frontend/app/controllers/cmor/legal/frontend/cookie_preferences_controller.rb
@@ -4,6 +4,8 @@ module Cmor
   module Legal
     module Frontend
       class CookiePreferencesController < Cmor::Legal::Frontend::Configuration.base_controller.constantize
+        include Rao::ResourcesController::LocationHistoryConcern
+
         if respond_to?(:before_action)
           before_action :initialize_resource, only: [:edit, :update]
         else
@@ -16,7 +18,7 @@ module Cmor
         def update
           respond_to do |format|
             if @resource.update_attributes(permitted_params)
-              format.html { redirect_to((request.referer || url_for(action: "edit")), notice: t(".success")) }
+              format.html { redirect_to(after_update_location, notice: t(".success")) }
             else
               format.html { render :edit }
             end
@@ -24,17 +26,22 @@ module Cmor
         end
 
         private
-          def permitted_params
-            if params.is_a?(ActiveSupport::HashWithIndifferentAccess)
-              params[:cookie_preferences]
-            else
-              params.fetch(:cookie_preferences, {}).permit(@resource.cookies.map(&:identifier))
-            end
-          end
 
-          def initialize_resource
-            @resource = ::Cmor::Legal::CookiePreferences.new(cookie_store: ::Cmor::Legal::CookieStore.new(send(::Cmor::Legal::Configuration.cookie_storage)))
+        def after_update_location
+          last_location
+        end
+
+        def permitted_params
+          if params.is_a?(ActiveSupport::HashWithIndifferentAccess)
+            params[:cookie_preferences]
+          else
+            params.fetch(:cookie_preferences, {}).permit(@resource.cookies.map(&:identifier))
           end
+        end
+
+        def initialize_resource
+          @resource = ::Cmor::Legal::CookiePreferences.new(cookie_store: ::Cmor::Legal::CookieStore.new(send(::Cmor::Legal::Configuration.cookie_storage)))
+        end
       end
     end
   end
