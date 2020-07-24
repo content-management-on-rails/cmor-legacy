c4c5a9fbeee19020822e0394d8c9a77d64923169
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-07-24 16:10:31 +0200

    Cmor::Files::Backednd: #cleanup.

--- cmor_files_backend/app/controllers/cmor/files/backend/attachments_controller.rb
+++ cmor_files_backend/app/controllers/cmor/files/backend/attachments_controller.rb
@@ -9,27 +9,6 @@ module Cmor
           ActiveStorage::Attachment
         end
 
-        # def destroy_many
-        #   @collection = load_collection_scope.where(id: params[:ids])
-        #   @collection.destroy_all
-
-        #   respond_with @collection, location: after_destroy_many_location, noticse: t('.success')
-        # end
-
-        # def publish_many
-        #   @collection = load_collection_scope.where(id: params[:ids])
-        #   @collection.map(&:publish!)
-
-        #   respond_with @collection, location: last_location, noticse: t('.success')
-        # end
-
-        # def unpublish_many
-        #   @collection = load_collection_scope.where(id: params[:ids])
-        #   @collection.map(&:unpublish!)
-
-        #   respond_with @collection, location: last_location, noticse: t('.success')
-        # end
-
         private
 
         def after_destroy_many_location
