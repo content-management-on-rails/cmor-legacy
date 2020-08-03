2df8dc9045d2c73c30faba23d83e71592418d2d6
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-08-03 10:55:41 +0200

    Cmor::Audits: Add batch actions to versions. Cmor::Audits: Fix exception on versions index table when item class was not present or not versioned anymore. Cmor::Audits: Add translations.

--- cmor_audits/config/routes.rb
+++ cmor_audits/config/routes.rb
@@ -2,6 +2,8 @@ Cmor::Audits::Engine.routes.draw do
   root to: 'home#index'
 
   namespace :paper_trail do
-    resources :versions
+    resources :versions do
+      post :destroy_many, on: :collection
+    end
   end
 end
