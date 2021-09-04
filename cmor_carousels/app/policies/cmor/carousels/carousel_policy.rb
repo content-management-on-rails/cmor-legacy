3b8ac35b424a4a47b7ae595167c49f64033392b1
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2021-09-04 13:00:49 +0200

    Cmor::Carousels: Update documentation.

---
+++ cmor_carousels/app/policies/cmor/carousels/carousel_policy.rb
@@ -0,0 +1,6 @@
+module Cmor
+  module Carousels
+    class CarouselPolicy < Cmor::Rbac::ResourcesPolicy::Base
+    end
+  end
+end
---
+++ cmor_carousels/app/policies/cmor/carousels/item_detail_policy.rb
@@ -0,0 +1,6 @@
+module Cmor
+  module Carousels
+    class ItemDetailPolicy < Cmor::Rbac::ResourcesPolicy::Base
+    end
+  end
+end
