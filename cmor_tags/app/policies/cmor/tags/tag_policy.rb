e22bed8646cc778196a4e96dba475660aef3ee8e
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2021-09-04 13:47:29 +0200

    Cmor::Tags: Update documentation. Cmor::Tags: Add access control policies.

---
+++ cmor_tags/app/policies/cmor/tags/tag_policy.rb
@@ -0,0 +1,4 @@
+module Cmor::Tags
+  class TagPolicy < Cmor::Rbac::ResourcesPolicy::Base
+  end
+end
---
+++ cmor_tags/app/policies/cmor/tags/tagging_policy.rb
@@ -0,0 +1,6 @@
+module Cmor
+  module Tags
+    class TaggingPolicy < Cmor::Rbac::ResourcesPolicy::Base
+    end
+  end
+end
