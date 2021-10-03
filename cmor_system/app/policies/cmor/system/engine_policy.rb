b657a5d017236ebf51a1f2dcfb1873b50ba0ff62
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2021-10-03 12:22:54 +0200

    Cmor::System: Add policies.

---
+++ cmor_system/app/policies/cmor/system/engine_policy.rb
@@ -0,0 +1,6 @@
+module Cmor
+  module System
+    class EnginePolicy < Cmor::Rbac::EnginePolicy::Base
+    end
+  end
+end
---
+++ cmor_system/app/policies/cmor/system/home_controller_policy.rb
@@ -0,0 +1,6 @@
+module Cmor
+  module System
+    class HomeControllerPolicy < Cmor::Rbac::HomeControllerPolicy::Base
+    end
+  end
+end
