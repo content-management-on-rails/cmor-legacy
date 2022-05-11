4e961ecd585c9e05fb54658a1579fead6ced48a0
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2022-05-11 14:29:09 +0200

    Cmor::Seo: enhance ui.

--- cmor_seo/config/routes.rb
+++ cmor_seo/config/routes.rb
@@ -7,6 +7,7 @@ Cmor::Seo::Engine.routes.draw do
   resources :meta_tags do
     acts_as_list
     acts_as_published
+    post :destroy_many, on: :collection
   end
   resources :generate_resource_classes_meta_tags_service, only: [:new, :create]
 
