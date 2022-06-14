bc0c6c59c313f169ecc35f96c2118f9e72b36aff
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2022-06-14 10:59:59 +0200

    Cmor::Core::Api: Improve docs.

--- cmor-core-api/app/controllers/concerns/cmor/core/api/controllers/token_authentication_concern.rb
+++ cmor-core-api/app/controllers/concerns/cmor/core/api/controllers/token_authentication_concern.rb
@@ -1,5 +1,27 @@
 module Cmor::Core::Api
   module Controllers
+    # You can secure endpoints so that they can only be accessed with a valid
+    # api token. This can be done by including the
+    # Cmor::Core::Api::Controllers::TokenAuthenticationConcern to the
+    # controller that should be secured:
+    #
+    #     class ApiController < ActionController::API
+    #       include Cmor::Core::Api::Controllers::TokenAuthenticationConcern
+    #       # ...
+    #     end
+    #
+    # Generate a new api token:
+    #
+    #     # rails console
+    #     > Cmor::Core::Api::ApiToken.create.token
+    #       => "<API_TOKEN>"
+    #
+    # To access secured controllers you have to provide a valid token via the
+    # Authorization Header:
+    #
+    #     GET http://localhost:3000/api/posts
+    #     Authorzation: Bearer <API_TOKEN>
+    #
     module TokenAuthenticationConcern
       extend ActiveSupport::Concern
 
