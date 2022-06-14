03b74963195a08e2edc503d3cfffacdd2616e21e
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2022-06-14 11:00:30 +0200

    Cmor::Core::Api: Fix boolean column missing default value.

--- cmor-core-api/db/migrate/20220603111637_create_cmor_core_api_api_tokens.rb
+++ cmor-core-api/db/migrate/20220603111637_create_cmor_core_api_api_tokens.rb
@@ -3,7 +3,7 @@ class CreateCmorCoreApiApiTokens < ActiveRecord::Migration[6.0]
     create_table :cmor_core_api_api_tokens do |t|
       t.string :token, null: false
       t.string :description
-      t.boolean :can_write
+      t.boolean :can_write, default: false
       t.timestamp :active_from, null: false
       t.timestamp :active_to, null: false
 
