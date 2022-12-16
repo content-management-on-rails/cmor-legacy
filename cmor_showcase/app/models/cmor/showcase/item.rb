f7974f614a111ee7cb85a19d5a9bdcdb7b66be67
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2022-12-16 18:40:44 +0100

    Cmor::Showcase: Fix appended post assets missing after upload.

--- cmor_showcase/app/models/cmor/showcase/item.rb
+++ cmor_showcase/app/models/cmor/showcase/item.rb
@@ -1,60 +1,68 @@
-module Cmor::Showcase
-  class Item < ApplicationRecord
-    include ActsAsPublished::ActiveRecord
-    include Markup::Rails::ActiveRecord
+module Cmor
+  module Showcase
+    class Item < ApplicationRecord
+      include ActsAsPublished::ActiveRecord
+      include Markup::Rails::ActiveRecord
 
-    belongs_to :category
+      belongs_to :category
 
-    acts_as_list scope: :category
-    default_scope -> { order(:category_id, :position) }
-    acts_as_published
+      acts_as_list scope: :category
+      default_scope -> { order(:category_id, :position) }
+      acts_as_published
 
-    # slugs
-    extend FriendlyId
-    friendly_id :name, use: :slugged
+      # slugs
+      extend FriendlyId
+      friendly_id :name, use: :slugged
 
-    acts_as_markup :body, Cmor::Core::Configuration.default_markup_options
+      acts_as_markup :body, Cmor::Core::Configuration.default_markup_options
 
-    validates :name, presence: true, uniqueness: { scope: [ :category_id ] }
+      validates :name, presence: true, uniqueness: { scope: [ :category_id ] }
 
-    module PreviewPictureConcern
-      extend ActiveSupport::Concern
+      module PreviewPictureConcern
+        extend ActiveSupport::Concern
 
-      def preview_picture
-        file_details.images.first
+        def preview_picture
+          file_details.images.first
+        end
       end
+
+      include PreviewPictureConcern
+
+      module FileDetailsConcern
+        extend ActiveSupport::Concern
+
+        included do
+          has_many :file_details, inverse_of: :item, dependent: :destroy, autosave: true
+        end
+
+        def file_details_count
+          file_details.count
+        end
+
+        def append_file_detail_assets=(collection)
+          clean_collection = collection.keep_if { |r| r.present? }
+          if clean_collection.any?
+            file_details << clean_collection.map { |r| file_details.build.tap{ |fd| fd.asset.attach(r) } }
+          end
+        end
+
+        def append_file_detail_assets
+          file_details
+        end
+
+        def overwrite_file_detail_assets=(collection)
+          clean_collection = collection.keep_if { |r| r.present? }
+          if clean_collection.any?
+            file_details.replace(clean_collection.map { |r| file_details.build.tap { |fd| fd.asset.attach(r) } })
+          end
+        end
+
+        def overwrite_file_detail_assets
+          file_details
+        end
+      end
+
+      include FileDetailsConcern
     end
-
-    include PreviewPictureConcern
-
-    module FileDetailsConcern
-      extend ActiveSupport::Concern
-
-      included do
-        has_many :file_details, inverse_of: :item, dependent: :destroy, autosave: true
-      end
-
-      def file_details_count
-        file_details.count
-      end
-
-      def append_file_detail_assets=(collection)
-        collection.map { |r| self.file_details.build.tap { |fd| fd.asset.attach(r); fd } }
-      end
-
-      def append_file_detail_assets
-        file_details
-      end
-
-      def overwrite_file_detail_assets=(collection)
-        file_details.replace(collection.map { |r| file_details.build.tap { |fd| fd.asset.attach(r); fd } })
-      end
-
-      def overwrite_file_detail_assets
-        file_details
-      end
-    end
-
-    include FileDetailsConcern
   end
 end
