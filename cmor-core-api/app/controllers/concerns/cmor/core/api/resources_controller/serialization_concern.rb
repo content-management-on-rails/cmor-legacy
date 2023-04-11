fc3dd6469139064616a9124c89aa408f3cc36f38
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-04-11 11:11:15 +0200

    Cmor::Core::Api: deprecate legacy populate param format.

--- cmor-core-api/app/controllers/concerns/cmor/core/api/resources_controller/serialization_concern.rb
+++ cmor-core-api/app/controllers/concerns/cmor/core/api/resources_controller/serialization_concern.rb
@@ -29,38 +29,38 @@ module Cmor
 
           end
 
-        def serialize_resource_for_collection(resource, populate_associations: true)
-          json = {}
-          json[:id] = resource.id
-          json[:attributes] = serializable_object(resource).as_json.except("id")
-          json[:attributes].merge!(populate(resource, json)) if populate_associations
-          json[:errors] = serialize_errors(resource.errors) if resource.errors.any?
-          json
-        end
-
-        def serialize_resource(resource, populate_associations: true)
-          json = { data: {} }
-          json[:data][:id] = resource.id
-          json[:data][:attributes] = serializable_object(resource).as_json.except("id")
-          json[:data][:attributes].merge!(populate(resource, json)) if populate_associations
-          json[:data][:errors] = serialize_errors(resource.errors) if resource.errors.any?
-          json
-        end
-
-        def serializable_object(resource)
-          if self.class.serializer_class.present?
-            self.class.serializer_class.new(resource)
-          else
-            resource
+          def serialize_resource_for_collection(resource, populate_associations: true)
+            json = {}
+            json[:id] = resource.id
+            json[:attributes] = serializable_object(resource).as_json.except("id")
+            json[:attributes].merge!(populate(resource, json)) if self.class.serializer_class.nil? && populate_associations
+            json[:errors] = serialize_errors(resource.errors) if resource.errors.any?
+            json
           end
-        end
+
+          def serialize_resource(resource, populate_associations: true)
+            json = { data: {} }
+            json[:data][:id] = resource.id
+            json[:data][:attributes] = serializable_object(resource).as_json.except("id")
+            json[:data][:attributes].merge!(populate(resource, json)) if populate_associations
+            json[:data][:errors] = serialize_errors(resource.errors) if resource.errors.any?
+            json
+          end
+
+          def serializable_object(resource)
+            if self.class.serializer_class.present?
+              self.class.serializer_class.new(resource, params.permit!.to_h)
+            else
+              resource
+            end
+          end
 
           def serialize_errors(errors)
             errors.as_json(full_messages: true)
           end
 
           def populate(resource, json)
-            [params[:populate]].compact.flatten.each_with_object({}) do |associated, memo|
+            normalize_populate_params(params[:populate]).each_with_object({}) do |associated, memo|
               record_or_association = resource.send(associated)
               if record_or_association.is_a?(ActiveRecord::Associations::CollectionProxy)
                 memo[associated] = serialize_collection(record_or_association, populate_associations: false)
@@ -68,6 +68,18 @@ module Cmor
                 memo[associated] = serialize_resource(record_or_association, populate_associations: false)
               end
             end
+          end
+
+          def normalize_populate_params(populate_param)
+            case populate_param
+            when String
+              ActiveSupport::Deprecation.warn("Passing a string to the populate parameter is deprecated. Please pass an array instead.")
+              [populate_param]
+            when ActionController::Parameters
+              populate_param.permit!.to_h.collect { |k, v| ActiveModel::Type::Boolean.new.cast(v) ? k : nil }
+            else
+              []
+            end.compact.flatten
           end
         end
       end
