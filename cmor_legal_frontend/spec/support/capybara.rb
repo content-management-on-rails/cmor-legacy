1a617fa8937690a7e0d2b3c9bae4305652c6337c
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-04-12 21:29:20 +0200

    Cmor::Legal::Frontend: Fix specs.

--- cmor_legal_frontend/spec/support/capybara.rb
+++ cmor_legal_frontend/spec/support/capybara.rb
@@ -2,8 +2,8 @@
 
 require "capybara"
 
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
-  end
-end
+# RSpec.configure do |config|
+#   config.before(:each, type: :system) do
+#     driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
+#   end
+# end
