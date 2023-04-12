85dfb944cd47b97e5bef423593f50979c62890ab
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-04-12 21:29:45 +0200

    Cmor::MultiTenancy::Frontend: Fix specs.

--- cmor_multi_tenancy_frontend/initialize_dummy.sh
+++ cmor_multi_tenancy_frontend/initialize_dummy.sh
@@ -1,50 +1,54 @@
 #!/bin/bash
 
-# Delete old dummy app
+# delete old dummy app
 rm -rf spec/dummy
 
-# Generate new dummy app
+# generate new dummy app
 DISABLE_MIGRATE=true bundle exec rake dummy:app
 
 if [ ! -d "spec/dummy/config" ]; then exit 1; fi
 
-# Cleanup
+# cleanup
 rm spec/dummy/.ruby-version
 rm spec/dummy/Gemfile
 
 cd spec/dummy
 
-# Use correct Gemfile
+# use correct Gemfile
 sed -i "s|../Gemfile|../../../Gemfile|g" config/boot.rb
 
-# Use Webpacker
-sed -i '17i\require "webpacker"' config/application.rb
+# setup webpacker
 rails webpacker:install
+sed -i "15irequire 'webpacker'" config/application.rb
 
-# Add ActiveStorage
+# setup simple form
+rails generate simple_form:install --bootstrap
+
+# setup active storage
 rails active_storage:install
 
-# I18n configuration
+# setup i18n
 touch config/initializers/i18n.rb
 echo "Rails.application.config.i18n.available_locales = [:en, :de]" >> config/initializers/i18n.rb
 echo "Rails.application.config.i18n.default_locale    = :de" >> config/initializers/i18n.rb
 
-# I18n routing
+# setup i18n routing
 touch config/initializers/route_translator.rb
 echo "RouteTranslator.config do |config|" >> config/initializers/route_translator.rb
 echo "  config.force_locale = true" >> config/initializers/route_translator.rb
 echo "end" >> config/initializers/route_translator.rb
 
-# Add turbolinks
+# setup turbolinks
 sed -i "15irequire 'turbolinks'" config/application.rb
 
+# setup cmor_multi_tenancy
 rails generate cmor:multi_tenancy:install
 rails cmor_multi_tenancy:install:migrations db:migrate db:test:prepare
 
-# Install
+# setup cmor_multi_tenancy_frontend
 rails generate cmor:multi_tenancy:frontend:install
 
-# Customize dummy app
+# setup dummy app
 rails generate controller Home index --no-helper --no-assets --no-request-specs --no-controller-specs --no-view-specs
 sed -i "2i  include Cmor::MultiTenancy::Controller::CurrentClientConcern" app/controllers/application_controller.rb
 sed -i "3i  around_action :set_current_client" app/controllers/application_controller.rb
