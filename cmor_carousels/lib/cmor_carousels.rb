de6eb10fe1f2966398294d0d323758a8ad3e3c54
Roberto Vasquez Angel <rva@beegoodit.de>
2023-08-22 07:12:18 +0200

    Cmor::Carousels: Fix dependencies.

--- cmor_carousels/lib/cmor_carousels.rb
+++ cmor_carousels/lib/cmor_carousels.rb
@@ -2,7 +2,7 @@ require 'active_storage_validations'
 require 'acts_as_list'
 require 'markup-rails'
 require 'acts_as_published'
-require 'cmor_core_frontend'
+require 'cmor-core-frontend'
 require 'mini_magick'
 require 'rao-view_helper'
 
