4ac7bf4c50fd46dbcbd6f757ae002bd85083e955
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-10-12 21:24:19 +0200

    Cmor::Testimonials: Fix specs.

--- cmor-testimonials/spec/factories/cmor/testimonials/testimonials.rb
+++ cmor-testimonials/spec/factories/cmor/testimonials/testimonials.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     role { "MyString" }
     company { "MyString" }
     body { "MyText" }
-    published_at { 1.hour.ago }
+    published { true }
     after(:build) do |resource|
       resource.image.attach(
         io: File.open(Cmor::Testimonials::Engine.root.join(*%w[spec files cmor testimonials testimonials homer.png])),
