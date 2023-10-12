fdfe3ac27a1082bafba752c1d6e7ead78cf44efc
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-10-12 21:06:05 +0200

    Cmor::Links::Api: Fix specs.

--- cmor-links-api/app/serializers/cmor/links/api/category_serializer.rb
+++ cmor-links-api/app/serializers/cmor/links/api/category_serializer.rb
@@ -1,19 +1,4 @@
 module Cmor::Links::Api
-  class CategorySerializer
-    def initialize(resource, params = {})
-      @resource = resource
-      @params = params
-    end
-
-    def as_json
-      @resource.as_json.merge(associations)
-    end
-
-    def associations
-      return {} unless @params[:populate]
-      @params[:populate].each_with_object({}) do |association_name, memo|
-        memo[association_name] = @resource.send(association_name)
-      end
-    end
+  class CategorySerializer < Cmor::Core::Api::Serializer::Base
   end
 end
