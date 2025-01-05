5ebd7f04c3234faa95b16c0ece8e852d6ed8a635
Roberto Vasquez-Angel <rva@beegoodit.de>
2025-01-05 02:00:40 +0100

    fix(Cmor::Rbac): active record not being loaded lazily

--- cmor_rbac/config/initializers/inject_user_concern.rb
+++ cmor_rbac/config/initializers/inject_user_concern.rb
@@ -1,12 +1,14 @@
 Rails.configuration.to_prepare do
-  begin
-    print "[CMOR RBAC] Including Model::Cmor::Rbac::UserConcern in #{Cmor::Rbac.user_class_name}"
-    Cmor::Rbac.user_class_name.constantize.class_eval do
-      include Model::Cmor::Rbac::UserConcern
-      puts " => [OK]"
+  ActiveSupport.on_load(:active_record) do
+    begin
+      print "[CMOR RBAC] Including Model::Cmor::Rbac::UserConcern in #{Cmor::Rbac.user_class_name}"
+      Cmor::Rbac.user_class_name.constantize.class_eval do
+        include Model::Cmor::Rbac::UserConcern
+        puts " => [OK]"
+      end
+    rescue NameError => e
+      puts " => [FAILED]"
+      puts "  Could not include Model::Cmor::Rbac::UserConcern into #{Cmor::Rbac.user_class_name}. Error: #{e.message}"
     end
-  rescue NameError => e
-    puts " => [FAILED]"
-    puts "  Could not include Model::Cmor::Rbac::UserConcern into #{Cmor::Rbac.user_class_name}. Error: #{e.message}"
   end
 end
